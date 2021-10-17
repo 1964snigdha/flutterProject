f08a149860074ea2584be41a93eba7305221270d
1964snigdha <1964snigdha@gmail.com>
2021-10-17 12:30:16 +0530

    testCase2:Gridview.builder()WithInfinityCount

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Singleton',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+        home: SafeArea(
+      child: Scaffold(
+        body: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2),
+            itemBuilder: (ctx, index) {
+              return Container(
+                color: Colors.red,
+                margin: const EdgeInsets.all(5),
+              );
+            }),
       ),
-      home: const MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    final ButtonStyle style =
-        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Flutter Singleton Template"),
-      ),
-      body: Column(children: <Widget>[
-        Container(
-          height: 100,
-          width: 800,
-          //alignment: Alignment.center,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10), color: Colors.green),
-          child: const Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              "A new 'ExampleInstance' has been created.",
-              style: TextStyle(color: Colors.white, fontSize: 25),
-            ),
-          ),
-        ),
-        const SizedBox(height: 20),
-        Container(
-          height: 100,
-          width: 800,
-          //alignment: Alignment.center,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10), color: Colors.green),
-          child: const Align(
-            alignment: Alignment.center,
-            child: Text(
-              "A new 'ExampleInstanceByDefinition' has been created.",
-              style: TextStyle(color: Colors.white, fontSize: 25),
-            ),
-          ),
-        ),
-        const SizedBox(height: 20),
-        Container(
-          //alignment: Alignment.center,
-          height: 100,
-          width: 800,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10), color: Colors.green),
-          child: const Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "A new 'ExampleStateWithoutSingleton' has been created.",
-              style: TextStyle(color: Colors.white, fontSize: 25),
-            ),
-          ),
-        ),
-        const SizedBox(height: 20),
-        ElevatedButton(
-          style: style,
-          onPressed: () {
-            print('Button #1 is clicked');
-          },
-          child: const Text('Change States text to  Singleton'),
-        ),
-        const SizedBox(height: 30),
-        ElevatedButton(
-          style: style,
-          onPressed: () {
-            print('Button #1 is clicked');
-          },
-          child: const Text('Reset'),
-        )
-      ]),
-    );
+    ));
   }
 }
