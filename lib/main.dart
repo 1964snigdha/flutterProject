3af934cb5dd18bf600b35bcfd3957787e202bdc0
1964snigdha <1964snigdha@gmail.com>
2021-10-17 13:01:38 +0530

    testCase3:Gridview.count()WithFixedNumberOfTilesInCrossAxis

--- lib/main.dart
+++ lib/main.dart
@@ -12,24 +12,96 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: SafeArea(
       child: Scaffold(
-        body: GridView.builder(
-            itemCount: 20,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2),
-            itemBuilder: (ctx, index) {
-              return Container(
-                color: Colors.red,
-                margin: const EdgeInsets.all(5),
-                child: Center(
-                  child: Text(
-                    index.toString(),
-                    style: const TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              );
-            }),
-      ),
+          body: GridView.count(
+        scrollDirection: Axis.horizontal,
+        crossAxisCount: 4,
+        children: [
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+          Container(
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+          ),
+        ],
+      )),
     ));
   }
 }
