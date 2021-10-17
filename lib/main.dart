f74a0a47d3cd43fd653db84ab77d646715e32065
1964snigdha <1964snigdha@gmail.com>
2021-10-17 14:12:53 +0530

    testCase5:StackwithPositioned alignment property of Stack has no effect on the child widget which is Positioned

--- lib/main.dart
+++ lib/main.dart
@@ -14,36 +14,32 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Flutter Stack Demo"),
         ),
-        body: Center(
-          child: Container(
-            width: 400,
-            height: 700,
-            color: Colors.grey,
-            child: Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                Container(
-                  width: 300,
-                  height: 300,
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Stack(
+            alignment: Alignment.centerLeft,
+            children: <Widget>[
+              Container(
+                width: 290,
+                height: 190,
+                color: Colors.green,
+              ),
+              Container(
+                width: 220,
+                height: 130,
+                color: Colors.yellow,
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  width: 150,
+                  height: 90,
                   color: Colors.red,
                 ),
-                Container(
-                  width: 200,
-                  height: 200,
-                  color: Colors.blue,
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.orange,
-                ),
-                Container(
-                  width: 50,
-                  height: 50,
-                  color: Colors.pink,
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
