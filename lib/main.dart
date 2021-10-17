ae5f3d3efe6e0aae799b92fc7c8a5ed228ae0a51
1964snigdha <1964snigdha@gmail.com>
2021-10-17 13:55:45 +0530

    testCase5:Stack

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,98 +10,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: SafeArea(
-      child: Scaffold(
-          body: GridView.count(
-        scrollDirection: Axis.horizontal,
-        crossAxisCount: 4,
-        children: [
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter Stack Demo"),
+        ),
+        body: Center(
+          child: Container(
+            width: 400,
+            height: 700,
+            color: Colors.grey,
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Container(
+                  width: 300,
+                  height: 300,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.orange,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.pink,
+                ),
+              ],
+            ),
           ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-          Container(
-            color: Colors.green,
-            margin: const EdgeInsets.all(5),
-          ),
-        ],
-      )),
-    ));
+        ),
+      ),
+    );
   }
 }
