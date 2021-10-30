bd78ad5ec9c8dbe3fb887569d62e238a5400d267
1964snigdha <1964snigdha@gmail.com>
2021-10-30 19:12:20 +0530

    TestCase2:ListView

--- lib/main.dart
+++ lib/main.dart
@@ -5,98 +5,41 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // It is the root widget of your application.
   @override
   Widget build(BuildContext context) {
+    const appTitle = 'Flutter Basic List Demo';
+
     return MaterialApp(
-      title: 'Flutter Demo Application',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const MyHomePage(title: 'Complex layout example'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text("Product List")),
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
         body: ListView(
-          padding: const EdgeInsets.fromLTRB(3.0, 12.0, 3.0, 12.0),
-          // ignore: prefer_const_literals_to_create_immutables
-          children: <Widget>[
-            const ProductBox(
-              name: "iPhone",
-              description: "iPhone is the top branded phone ever",
-              price: 55000,
-              image: '',
+          children: const <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
             ),
-            const ProductBox(
-                name: "Android",
-                description: "Android is a very stylish phone",
-                price: 10000,
-                image: "android.png"),
-            const ProductBox(
-                name: "Tablet",
-                description: "Tablet is a popular device for official meetings",
-                price: 25000,
-                image: "tablet.png"),
-            const ProductBox(
-                name: "Laptop",
-                description: "Laptop is most famous electronic device",
-                price: 35000,
-                image: "laptop.png"),
-            const ProductBox(
-                name: "Desktop",
-                description: "Desktop is most popular for regular use",
-                price: 10000,
-                image: "computer.png"),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
+            ListTile(
+              leading: Icon(Icons.contacts),
+              title: Text('Contact'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Setting'),
+            ),
           ],
-        ));
-  }
-}
-
-class ProductBox extends StatelessWidget {
-  const ProductBox(
-      {Key? key,
-      required this.name,
-      required this.description,
-      required this.price,
-      required this.image})
-      : super(key: key);
-  final String name;
-  final String description;
-  final int price;
-  final String image;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.all(2),
-        height: 110,
-        child: Card(
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-              Image.asset("assets/" + image),
-              Expanded(
-                  child: Container(
-                      padding: const EdgeInsets.all(5),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Text(name,
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.bold)),
-                          Text(description),
-                          Text("Price: " + price.toString()),
-                        ],
-                      )))
-            ])));
+        ),
+      ),
+    );
   }
 }
