5ea4ee226c28917128182d65a173f6029c8d3b61
1964snigdha <1964snigdha@gmail.com>
2021-10-30 19:44:42 +0530

    TestCase3:IndexedStackDemo

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,80 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    const appTitle = 'Flutter Basic List Demo';
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Indexed Demo',
+      home: HomePage(),
+    );
+  }
+}
 
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(appTitle),
-        ),
-        body: ListView(
-          children: const <Widget>[
-            ListTile(
-              leading: Icon(Icons.map),
-              title: Text('Map'),
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int index = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Subhadip')),
+      body: Center(
+        child: IndexedStack(
+          index: index,
+          children: [
+            // Index = 0
+            Container(
+              width: 400,
+              height: 400,
+              color: Colors.amber,
+              alignment: Alignment.center,
+              child: const Text(
+                '0',
+                style: TextStyle(fontSize: 100),
+              ),
             ),
-            ListTile(
-              leading: Icon(Icons.photo_album),
-              title: Text('Album'),
+            // Index = 1
+            Container(
+              width: 400,
+              height: 400,
+              color: Colors.purple,
+              alignment: Alignment.center,
+              child: const Text('1', style: TextStyle(fontSize: 100)),
             ),
-            ListTile(
-              leading: Icon(Icons.phone),
-              title: Text('Phone'),
-            ),
-            ListTile(
-              leading: Icon(Icons.contacts),
-              title: Text('Contact'),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Setting'),
-            ),
+            // Index = 2
+            Container(
+              width: 400,
+              height: 400,
+              color: Colors.red,
+              alignment: Alignment.center,
+              child: const Text('2', style: TextStyle(fontSize: 100)),
+            )
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.refresh_rounded),
+        onPressed: () {
+          setState(() {
+            if (index == 2) {
+              index = 0;
+            } else {
+              index = index + 1;
+            }
+          });
+        },
       ),
     );
   }
